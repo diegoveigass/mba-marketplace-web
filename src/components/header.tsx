0ac1bd27b1d6ed587799f0f4480d28a257c12968
Diego Veiga <diego.veiga@xmobots.com.br>
2024-10-15 08:58:27 -0300

    feat: logout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,36 @@
-import { ChartHistogramIcon, PackageIcon, PlusSignIcon } from 'hugeicons-react'
-import { Link, NavLink } from 'react-router-dom'
+import {
+  ChartHistogramIcon,
+  Logout01Icon,
+  PackageIcon,
+  PlusSignIcon,
+} from 'hugeicons-react'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 import logoImg from '../assets/logo.svg'
+import { useSignOutControllerHandle } from '../api/sessions/sessions'
+import axios, { type AxiosError } from 'axios'
+import { toast } from 'sonner'
 
 export function Header() {
+  const navigate = useNavigate()
+  const { mutateAsync: signOut } = useSignOutControllerHandle()
+
+  async function handleSignOut() {
+    try {
+      await signOut()
+      navigate('/sign-in')
+    } catch (err) {
+      const errors = err as Error | AxiosError
+
+      if (axios.isAxiosError(errors)) {
+        toast.error(errors.response?.data.message)
+        return
+      }
+      throw new Error(errors.message)
+    }
+  }
+
   return (
     <header className="w-full flex justify-between items-center py-4 px-5">
       <img src={logoImg} alt="" className="w-14 h-10" />
@@ -38,13 +65,31 @@ export function Header() {
           Novo produto
         </Link>
 
-        <div className="size-12">
-          <img
-            src="https://github.com/diegoveigass.png"
-            className="rounded-lg"
-            alt=""
-          />
-        </div>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button type="button" className="size-12 outline-none">
+              <img
+                src="https://github.com/diegoveigass.png"
+                className="rounded-lg"
+                alt=""
+              />
+            </button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content
+              className="min-w-[120px] rounded-md bg-shape p-1"
+              sideOffset={5}
+            >
+              <DropdownMenu.Item
+                className="text-sm flex items-center justify-between px-4 py-2 h-[25px] text-red-900 select-none rounded-md outline-none data-[highlighted]:bg-red-400"
+                onClick={handleSignOut}
+              >
+                Logout
+                <Logout01Icon className="size-4" />
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
       </div>
     </header>
   )
